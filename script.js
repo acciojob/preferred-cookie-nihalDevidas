22c848e71991edc2a90320bc8d8495a8c397a036
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-09-13 23:05:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ function setCookiePreference(){
 		({...accumulator,[key.trim()]: decodeURIComponent(value)  }),{});
 	console.log(cookieObj);
 
-	let font_size = cookieObj.Fsize;
-	let font_color= cookieObj.calar;
+	let font_size = cookieObj.fontsize;
+	let font_color= cookieObj.fontcolor;
 
 	document.getElementById("fontcolor").value = font_color;
 	document.getElementById("fontsize").value = font_size;
+
+	document.body.style= `font-size:${font_size}px; color:${font_color}`;
 
 }
 
@@ -25,8 +27,8 @@ form1.addEventListener('submit',(e)=>{
 	const colorValue = document.getElementById("fontcolor").value;
 	const sizeValue = document.getElementById("fontsize").value;
 	
-	document.cookie = `calar=${colorValue}; expires= mon, 14 oct 2023 12:00:00 UTC; path=/`;
-	document.cookie = `Fsize=${sizeValue}; expires= mon, 14 oct 2023 12:00:00 UTC; path=/`;
+	document.cookie = `fontcolor=${colorValue}; expires= mon, 14 oct 2023 12:00:00 UTC; path=/`;
+	document.cookie = `fontsize=${sizeValue}; expires= mon, 14 oct 2023 12:00:00 UTC; path=/`;
 	
 });
 setCookiePreference();
